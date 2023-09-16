326c7fc5a7bd6cea862c21191516459fef080c86
Jimmy Yang <jimmyy1337@gmail.com>
2023-09-16 15:35:40 -0400

    Add components

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,3 +2,4 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
+<a href="/route1" class="underline">click to route 1</a>
---
+++ frontend/src/routes/route1/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Input from '$lib/components/Input.svelte';
+</script>
+
+<p>this is the route1 page access through url /route1</p>
+<p>use nextjs-like folder structure to structure routes</p>
+
+<Input />
