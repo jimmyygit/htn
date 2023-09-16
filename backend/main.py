78944daa90f3853691c2832313cdb48292a6b244
Jimmy Yang <jimmyy1337@gmail.com>
2023-09-16 16:35:41 -0400

    Add cohere

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import cohere
 
 app = FastAPI()
+co = cohere.Client("")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/cohere")
+async def root():
+    response = co.generate(
+        prompt="Please explain to me how LLMs work",
+    )
+    return {"message": response}
