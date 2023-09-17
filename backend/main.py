f2451a34099623f9d629288890e571fd40c040a1
hairanliang <65080149+hairanliang@users.noreply.github.com>
2023-09-17 06:34:23 -0400

    Update main.py
    Trying command light for category

--- backend/main.py
+++ backend/main.py
@@ -67,7 +67,7 @@ class Category(BaseModel):
 async def categorize(category: Category):
     BASE_PROMPT = f"""We have the following categories: {", ".join(category.categories)}. Which of these categories would you classify the following message under: {category.note}."""
 
-    response = co.generate(model="command", prompt=BASE_PROMPT)
+    response = co.generate(model="command-light", prompt=BASE_PROMPT)
     return_category = response.generations[0].text.strip()
     return {"category": return_category}
 
@@ -75,7 +75,7 @@ async def categorize(category: Category):
 @app.post("/suggestion")
 async def suggestion(prompt: Suggestion):
     SUGG_PROMPT = f""" Based on this note: {prompt.note}, provide 1 very brief and very funny suggestion on how to accomplish the aforementioned message.
-    Make sure that suggestion is VERY funny, but not mean to anyone--they should be harmless jokes and not contain any violence. The suggest must just be one short sentence."""
+    Make sure that suggestion is VERY funny, but not mean to anyone--they should be harmless jokes and not contain any violence. The suggestion must just be one short sentence."""
     suggestion = co.generate(max_tokens = 100, model="command", prompt=SUGG_PROMPT)
     return_suggestion = suggestion.generations[0].text.strip()
     return {"suggestion": return_suggestion}
