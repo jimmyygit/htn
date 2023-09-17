061ffe7e6b6305a1b1df6c117355c535e2b085e1
Jimmy Yang <jimmyy1337@gmail.com>
2023-09-17 02:28:56 -0400

    Add static gen

--- frontend/src/routes/route1/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import Input from '$lib/components/Input.svelte';
-</script>
-
-<p>this is the route1 page access through url /route1</p>
-<p>use nextjs-like folder structure to structure routes</p>
-
-<Input />
